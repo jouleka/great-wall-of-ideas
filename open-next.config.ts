df4153dfa2e935dfbc7b2a621d5afd5780523853
Jurgen <lekacoding@gmail.com>
2025-12-05 11:57:59 +0100

    fix: remove invalid minify option from open-next config

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,6 +1,3 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-export default defineCloudflareConfig({
-  // Enable minification for smaller worker bundles
-  minify: true,
-});
+export default defineCloudflareConfig({});
